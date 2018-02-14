0ef723f05745610cf745852952124dde7463ea53
Brijith <brijithb@gmail.com>
2018-02-14 12:18:56 +0530

    dfS code cleanup

--- graphs/src/graphs/DFSTraversal.java
+++ graphs/src/graphs/DFSTraversal.java
@@ -19,16 +19,7 @@ public class DFSTraversal <T> {
 		
 	}
 public static void main(String []args){
-	//Graph<Integer> graph= new Graph<>();
 	Graph<Integer> graph = new Graph<>();
-	/*graph.addEdge(0, 3);
-	graph.addEdge(1, 3);
-	graph.addEdge(3, 4);
-	graph.addEdge(2, 4);
-	graph.addEdge(1, 2);
-	graph.addEdge(0, 3);
-	graph.addEdge(3, 1);*/
-	
 	graph.addEdge(0, 1);
     graph.addEdge(0, 2);
     graph.addEdge(1, 2);
